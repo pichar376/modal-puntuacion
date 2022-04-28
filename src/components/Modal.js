912f7887f34eda82e8b80409ba3b8c664191b755
pichar376 <96349975+pichar376@users.noreply.github.com>
2022-04-27 23:56:07 -0500

    En esta ocasion se agregaron mas estlos al modal como colores,hover y se creo un componente para la puntuacion

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,32 @@
+import { useState } from "react";
+import Puntuacion from "./Puntuacion";
+
 const Modal = () => {
+  const [puntuacion, setPuntuacion] = useState("");
   return (
     <div className="modal-content">
+      <article className="modal">
+        <span className="circle-star"> <article className="svg">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614" /></svg>
 
-      <article className={"modal"}>
-        <span>logo</span>
+        </article>
+        </span>
         <span className="how">How did we do?</span>
         <p>please let us know how we did with your support <br />
           request.All feed back is appreciated to help us<br />improve our offering!</p>
-        <section className="puntuacion">
-          <span class="circle">1</span><span class="circle">2</span><span class="circle">3</span><span class="circle">4</span><span class="circle">5</span>
-        </section>
-        <nav>
-          <input type="submit" value="enviar" />
-        </nav>
+
+        <form>
+
+          <Puntuacion />
+          <nav>
+            <input type="submit" value="submit" />
+          </nav>
+        </form>
+
+
 
       </article >
-    </div>
+    </div >
 
   );
 }
---
+++ src/components/Puntuacion.js
@@ -0,0 +1,10 @@
+const Puntuacion = () => {
+  return (
+    <section className="puntuacion">
+      <button className="circle">1</button><button className="circle">2</button><button className="circle">3</button><button className="circle">4</button><button className="circle">5</button>
+
+    </section>
+  );
+}
+
+export default Puntuacion;
